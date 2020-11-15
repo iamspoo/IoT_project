f6a5e48b0bd750db1f1dc0fe507ba08725ecc69c
iamspoo <spoorthi246@gmail.com>
2020-11-15 19:37:32 +0530

    commit

--- iot_project/light/urls.py
+++ iot_project/light/urls.py
@@ -8,5 +8,6 @@ path('signup/',views.signup,name='signup'),
 path('adminpage/',views.adminview,name='adminpage'),
 path('emppage/',views.empview,name='emppage'),
 path('register/',views.register,name='register'),
-
+path('adminsignin/',views.adminsignin,name='adminsignin'),
+path('empsignin/',views.empsignin,name='empsignin'),
 ]
--- iot_project/light/views.py
+++ iot_project/light/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render
 from .models import light,area
 from .forms import EmpForm
 from django.http import HttpResponseRedirect,HttpResponse,JsonResponse
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 # Create your views here.
-
-def signup(request):
-    return render(request, 'register.html')
-
-
 
 def adminview(request):
     l=light.objects.all()
@@ -39,3 +34,42 @@ def empview(request):
         areaobj=area.objects.get(areacode=i.areafk_id)
         i.address=areaobj.address
     return render(request,'light/emp_page.html',{"lights":l})
+    
+def adminsignin(request):
+    admin=None
+    uname=request.POST.get("username")
+    password=request.POST.get("password")
+    admin=User.objects.filter(username=uname)
+    if not admin or admin[0].is_superuser!=1:
+        return render(request,'home.html',{'m':'Acess Denied, Not a admin'})
+    else:
+        if admin[0].password == password:
+            request.session['adminid']=admin[0].id
+            return HttpResponseRedirect('/light/adminpage')
+        else:
+            return render(request,'home.html',{'m':'Invalid username or password'})
+            
+            
+def empsignin(request):
+    user=None
+    uname=request.POST.get("username")
+    password=request.POST.get("password")
+    user=User.objects.filter(username=uname)
+    '''user=authenticate(request,username=uname,password=password)
+    login(request,user)'''
+    if not user or user[0].password != password:
+        return render(request,'home.html',{'m':'Invalid username or password'})
+    else:
+        request.session['userid']=user[0].id
+        return HttpResponseRedirect('/light/emppage')
+  
+    
+            
+            
+            
+            
+            
+            
+            
+            
+            
